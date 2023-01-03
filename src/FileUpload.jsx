8e7ead981ead7569e45b59adbd14d72a48542d2f
stefan <stefan@stefan.com>
2023-01-03 16:15:54 +0100

    finished frontend

--- src/FileUpload.jsx
+++ src/FileUpload.jsx
@@ -6,22 +6,32 @@ function FileUpload() {
 
   const changeHandler = (ev) => {
     setSelectedFile(ev.target.files[0]);
-    setSelectedFile(true);
+    setIsFilePicked(true);
   };
   const submitHandler = () => {
     const formData = new FormData();
+    formData.append("File", selectedFile);
+    fetch("https://httpbin.org/post", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success!", result);
+      })
+      .catch((err) => console.log("Error", err));
   };
+  console.log(selectedFile);
   return (
     <div>
       <input onChange={changeHandler} type="file" name="file" />
-      {selectedFile ? (
+      {isFilePicked ? (
         <>
           <p>Filename : {selectedFile.name}</p>
           <p>Filetype : {selectedFile.type}</p>
           <p>Size in bytes : {selectedFile.size}</p>
           <p>
-            Last modified date:{" "}
-            {selectedFile.lastModifiedDate.toLocaleDateString()}
+            {/* Last modified date: {new Date(selectedFile.lastModified * 1000)} */}
           </p>
         </>
       ) : (
