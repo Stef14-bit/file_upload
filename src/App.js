9fd28eed28035ff3d5f926fd564e44fb89cfd40c
stefan <stefan@stefan.com>
2023-01-03 15:44:23 +0100

    basic upload component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import FileUpload from "./FileUpload";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FileUpload />
+    </div>
+  );
 }
 
 export default App;
